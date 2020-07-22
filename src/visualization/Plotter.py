41b607d506ed0d9b2365a06a8227d73f995a6df4
Murilo <43999445+murilobellatini@users.noreply.github.com>
2020-07-22 18:36:34 -0300

    Update Plotter.py

--- src/visualization/Plotter.py
+++ src/visualization/Plotter.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
 stopwords = set(STOPWORDS)
 
 def show_wordcloud(data, title = None):
@@ -32,3 +37,27 @@ def plot_nyt_word_incidence(df:pd.DataFrame, lookup_word:str, freq='M'):
                 figsize=(12,6),
                 title=f'Mentions About {lookup_word.title()} Over Time')\
         .set(xlabel='date', ylabel='article percentage')
+
+
+def plot_confusion_matrix(model, df:pd.DataFrame, features:np.array, labels:pd.Series, category_to_id:dict):
+    """
+    Trains and plots confusion matrix of model fitted to df with features and labels. 
+    Returns fitted model, corresponding confusion matrix, y_test and y_pred.
+    """
+    X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    conf_mat = confusion_matrix(y_test, y_pred)
+
+    fig, ax = plt.subplots(figsize=(10,8))
+
+    sns.heatmap(conf_mat, annot=True, fmt='d',
+                xticklabels=category_to_id.keys(), yticklabels=category_to_id.keys())
+
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title(f'Confusion Matrix - {model.__class__.__name__} Text Classifier')
+
+    plt.show()
+
+    return model, conf_mat, y_test, y_pred, indices_test
