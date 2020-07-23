b8a09e71d8c7ec1593f5ed53af29b0eb4cd8f647
Murilo <43999445+murilobellatini@users.noreply.github.com>
2020-07-23 13:55:19 -0300

    Create Loader.py for GloVe vectors

---
+++ src/extracting/Loader.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pathlib as pl 
+from tqdm import tqdm
+
+def load_glove_model(path:pl.Path):
+    print("Loading Glove Model")
+    f = open(path,'r', encoding='utf8')
+    gloveModel = {}
+    for line in tqdm(f):
+        splitLines = line.split()
+        word = splitLines[0]
+        wordEmbedding = np.array([float(value) for value in splitLines[1:]])
+        gloveModel[word] = wordEmbedding
+    print(len(gloveModel)," words loaded!")
+    return gloveModel
